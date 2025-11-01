88176466b9e8570756a0d5d0e0a2bb0ce56f64ad
nikita <nikitaorlov59@gmail.com>
2025-11-01 14:58:43 +0500

    1.9

--- l4/Collections.cs
+++ l4/Collections.cs
@@ -18,15 +18,6 @@ namespace l4
         //без метода Reverse
         public void ReverseList()
         {
-            List<int> rev_list = new List<int>(list.Count);
-
-            int ind_rev = 0;
-
-            for (int i = list.Count - 1; i >= 0; i--)
-            {
-                rev_list.Insert(ind_rev++, list[i]);
-            }
-
             Console.WriteLine("Исходный список:");
             foreach (int num in list)
             {
@@ -34,8 +25,16 @@ namespace l4
             }
             Console.WriteLine();
 
+            List<int> list_copy = new List<int>(list);
+            int ind_rev = 0;
+
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                list[ind_rev++] = list_copy[i];
+            }
+
             Console.WriteLine("Перевернутый список:");
-            foreach (int num in rev_list)
+            foreach (int num in list)
             {
                 Console.Write(num + " ");
             }
