c04b303a00c459e9bf363b2d8eed6b11cab53d30
nikita <nikitaorlov59@gmail.com>
2025-11-01 13:30:00 +0500

    Добавьте файлы проекта.

---
+++ l4/Collections.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l4
+{
+    internal class Collections
+    {
+        private List<int> list;
+
+        public Collections(List<int> list)
+        {
+            this.list = list; 
+        }
+
+        //без метода Reverse
+        public void ReverseList()
+        {
+            List<int> rev_list = new List<int>(list.Count);
+
+            int ind_rev = 0;
+
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                rev_list.Insert(ind_rev++, list[i]);
+            }
+
+            Console.WriteLine("Исходный список:");
+            foreach (int num in list)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Перевернутый список:");
+            foreach (int num in rev_list)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+        }
+
+        //с методом Reverse
+        //public void RevList()
+        //{
+        //    Console.WriteLine("Исходный список:");
+        //    foreach (int num in list)
+        //    {
+        //        Console.Write(num + " ");
+        //    }
+        //    Console.WriteLine();
+
+        //    list.Reverse();
+
+        //    Console.WriteLine("Перевернутый список:");
+        //    foreach (int num in list)
+        //    {
+        //        Console.Write(num + " ");
+        //    }
+        //    Console.WriteLine();
+        //}
+
+
+    }
+}
---
+++ l4/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace l4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.9
+            Console.WriteLine("1.9");
+            Console.Write("Введите длину списка: ");
+            int len_list = Convert.ToInt32(Console.ReadLine());
+            List<int> lst = new List<int>();
+
+            for (int i = 0; i < len_list; i++)
+            {
+                Console.Write("Введите эл-т списка под индексом " + i + ": ");
+                lst.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            Collections col1 = new Collections(lst);
+            col1.ReverseList();
+
+            //col1.RevList();
+
+        }
+    }
+}
