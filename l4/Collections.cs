fa6ec5376dad93b22e5a1c3ff8760dfd1125a99a
nikita <nikitaorlov59@gmail.com>
2025-11-07 21:24:27 +0500

    3.9, 4.9, 5.9

--- l4/Collections.cs
+++ l4/Collections.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -92,7 +94,7 @@ namespace l4
                     lst.AddBefore(node, E);
                     Console.WriteLine("По индексу " + indE + " вставлен эл-т E: " + E);
                 }
-                else 
+                else
                 {
                     Console.Write("Введите эл-т списка под индексом " + i + ": ");
                     lst.AddLast(Convert.ToInt32(Console.ReadLine()));
@@ -132,6 +134,207 @@ namespace l4
                 Console.Write(item + " ");
             }
             Console.WriteLine();
+        }
+
+
+        //3.9
+        private static HashSet<string> CreateHashSet(int num_disc)
+        {
+            HashSet<string> set = new HashSet<string>();
+            Console.WriteLine("Введите кол-во студентов дискотеки " + num_disc + ": ");
+            int len_set = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < len_set; i++)
+            {
+                Console.Write("Введите " + (i + 1) + " студента: ");
+                set.Add(Console.ReadLine());
+            }
+
+            return set;
+        }
+
+
+        public static void Discos(int count_discos)
+        {
+            //массив множеств дискотек
+            HashSet<string>[] discs = new HashSet<string>[count_discos];
+            //создание множеств дискотек
+            for (int i = 0; i < discs.Length; i++)
+            {
+                discs[i] = CreateHashSet(i + 1);
+            }
+
+            HashSet<string> students = new HashSet<string>();
+
+            //заполнение множества студентов из множеств дискотек
+            for (int i = 0; i < discs.Length; i++)
+            {
+                foreach (string stud in discs[i])
+                {
+                    students.Add(stud);
+                }
+                Console.WriteLine();
+            }
+
+            //вывод множеств
+            for (int i = 0; i < discs.Length; i++)
+            {
+                Console.Write("Дискотека " + (i + 1) + ": ");
+                foreach (string stud in discs[i])
+                {
+                    Console.Write(stud + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Студенты: ");
+            foreach (string stud in students)
+            {
+                Console.Write(stud + " ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < discs.Length; i++)
+            {
+                discs[i].IntersectWith(students);
+
+                if (discs[i].Count == students.Count)
+                {
+                    Console.WriteLine("На дискотеку " + (i + 1) + " ходили все студенты группы");
+                }
+                else if (discs[i].Count == 0)
+                {
+                    Console.WriteLine("На дискотеку " + (i + 1) + " не ходил ни один из студентов группы");
+                }
+                else if (discs[i] != students)
+                {
+                    Console.WriteLine("На дискотеку " + (i + 1) + " ходили некоторые студенты группы");
+                }
+
+            }
+
+        }
+
+        //4.9
+        public static void Symbols()
+        {
+            Console.WriteLine("Введите текст для записи в файл: ");
+            string text = Console.ReadLine();
+
+            using (FileStream stream_write = new FileStream("russian_text.txt", FileMode.Create))
+            {
+                byte[] text_arr = Encoding.Default.GetBytes(text);
+                stream_write.Write(text_arr, 0, text_arr.Length);
+            }
+            using (FileStream stream_read = File.OpenRead("russian_text.txt"))
+            {
+                byte[] text_arr = new byte[stream_read.Length];
+                stream_read.Read(text_arr, 0, text_arr.Length);
+
+                string textFromFile = Encoding.Default.GetString(text_arr);
+                Console.WriteLine("Содержимое файла: " + textFromFile);
+
+                string[] words = textFromFile.Split(new char[] { ' ' });
+                SortedSet<char> syms = new SortedSet<char>();
+
+                for (int i = 1; i < words.Length; i += 2)
+                {
+                    foreach (char sym in words[i])
+                    {
+                        syms.Add(sym);
+                    }
+                }
+
+                Console.WriteLine("Символы, которые встречаются хотя бы однажды в словах с чётными номерами: ");
+                foreach (char sym in syms)
+                {
+                    Console.Write(sym + " ");
+                }
+                Console.WriteLine();
+
+            }
+        }
+
+        //5.9
+        private static void CreateFileForApplicants()
+        {
+            Console.WriteLine("Введите кол-во абитуриетов, принимавших участие в тестировании(N): ");
+            string N = Console.ReadLine();
+
+            string path = "applicants.txt";
+
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                writer.WriteLine(N);
+
+                Console.WriteLine("Введите N строк <Фамилия><Имя><Баллы>\n" +
+                        "Где <Фамилия> – строка, состоящая не более чем из 20 символов;\n" +
+                        "<Имя> – строка, состоящая не более чем из 15 символов,\n" +
+                        "<Баллы> – строка, содержащая два целых числа,\nразделенных пробелом – баллы, полученные на тестировании по каждому из трёх предметов." +
+                        "При этом <Фамилия> и <Имя>, <Имя> и <Баллы> разделены одним пробелом.\n" +
+                        "Пример входной строки:\nРоманов Вельямин 48 39 55");
+                for (int i = 0; i < Convert.ToInt32(N); i++)
+                {
+                    Console.Write("Введите " + (i+1) + " строку: ");
+                    string line = Console.ReadLine();
+                    writer.WriteLine(line);
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+        public static void Admission()
+        {
+            CreateFileForApplicants();
+
+            string path = "applicants.txt";
+
+            SortedList<string, string> applicants = new SortedList<string, string>();
+
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                int N = Convert.ToInt32(reader.ReadLine());
+                for (int i = 0; i < N; i++)
+                {
+                    string appl_str = reader.ReadLine();
+                    string[] appl_data = appl_str.Split(new char[] { ' ' });
+                    applicants.Add(String.Join(" ", appl_data[..2]), String.Join(" ", appl_data[2..]));
+                }
+            }
+
+            Console.WriteLine("Допущенные к сдаче экзаменов: ");
+            foreach (string key in applicants.Keys)
+            {
+                string points_str = applicants[key];
+                string fl = key;
+                string[] points = points_str.Split(new char[] { ' ' });
+                int sum_points = 0;
+
+                bool failed = false;
+                foreach (string point in points)
+                {
+                    int int_point = Convert.ToInt32(point);
+                    if (int_point < 30)
+                    {
+                        failed = true;
+                        break;
+                    }
+                    sum_points += int_point;
+                }
+
+                if (!failed)
+                {
+                    if (sum_points >= 140) 
+                    {
+                        Console.WriteLine(key);
+                    }
+                    
+                }
+
+
+            }
+
         }
     }
 }
--- l4/Program.cs
+++ l4/Program.cs
@@ -4,21 +4,35 @@
     {
         static void Main(string[] args)
         {
-            //1.9
-            Console.WriteLine("1.9");
-            Collections.ReverseList();
+            ////1.9
+            //Console.WriteLine("1.9");
+            //Collections.ReverseList();
 
-            //Collections.RevList();
+            ////Collections.RevList();
 
-            //2.9
-            Console.WriteLine("2.9");
-            Console.WriteLine("Введите элемент F: ");
-            int F = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите элемент E слева и справа от которого нужно вставить элемент F: ");
-            int E = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите индекс, на который нужно поместить эл-т E");
-            int indE = Convert.ToInt32(Console.ReadLine());
-            Collections.Insert(F, E, indE);
+            ////2.9
+            //Console.WriteLine("2.9");
+            //Console.WriteLine("Введите элемент F: ");
+            //int F = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите элемент E слева и справа от которого нужно вставить элемент F: ");
+            //int E = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите индекс, на который нужно поместить эл-т E");
+            //int indE = Convert.ToInt32(Console.ReadLine());
+            //Collections.Insert(F, E, indE);
+
+            ////3.9
+            //Console.WriteLine("3.9");
+            //Console.WriteLine("Введите кол-во дискотек: ");
+            //int count_discos = Convert.ToInt32(Console.ReadLine());
+            //Collections.Discos(count_discos);
+
+            //4.9
+            Console.WriteLine("4.9");
+            Collections.Symbols();
+
+            //5.9
+            Console.WriteLine("5.9");
+            Collections.Admission();
         }
     }
 }
