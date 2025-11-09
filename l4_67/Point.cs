381403f8e988d05ad65a1ed5d1616af35936ff94
nikita <nikitaorlov59@gmail.com>
2025-11-09 16:44:38 +0500

    6.9

---
+++ l4_67/Point.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l4_67
+{
+    internal class Point
+    {
+        private double _x, _y;
+
+        public Point(double x, double y)
+        {
+            this._x = x;
+            this._y = y;
+        }
+
+        public double Interval()
+        {
+            Console.Write("Расстояние от точки до начала координат: ");
+            return Math.Sqrt(_x * _x + _y * _y);
+        }
+
+        public static Point operator --(Point point)
+        {
+            Console.WriteLine("Координаты x и y уменьшены на 1");
+            return new Point(--point._x, --point._y);
+        }
+
+        public static Point operator -(Point point)
+        {
+            Console.WriteLine("Координаты x и y переставлены");
+            return new Point(point._y, point._x);
+        }
+
+        public static implicit operator int(Point p)
+        {
+            Console.Write("Целая часть координаты x: ");
+            return (int)p._x;
+        }
+
+        public static explicit operator double(Point p)
+        {
+            Console.Write("Ккоордината y: ");
+            return (double)p._y;
+        }
+
+        public override string ToString()
+        {
+            return "Координаты точки: x = " + _x + " y = " + _y;
+        }
+
+    }
+}
---
+++ l4_67/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace l4_67
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите координату x: ");
+                double x = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите координату y: ");
+                double y = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine();
+
+                Point point = new Point(x, y);
+
+                Console.WriteLine(point.ToString()); // Метод ToString
+                Console.WriteLine(point.Interval());
+
+                // Унарные операции
+                // Операция --
+                Point point_lower = new Point(x, y);
+                point = --point_lower;
+                Console.WriteLine(point.ToString());
+
+                // Операция -
+                Point point_change = new Point(x, y);
+                point = -point_change;
+                Console.WriteLine(point.ToString());
+
+                // Операции приведения типа
+                // Неявное приведение к Int
+                Point point_type = new Point(x, y);
+                int x_int = point_type;
+                Console.WriteLine(x_int);
+
+                // Явное приведение к Double
+                double y_double = (double)point_type;
+                Console.WriteLine(y_double);
+
+                // Бинарные операции
+                // – Point p, целое число (левосторонняя операция, уменьшается координата х)
+                Point point_leftx = new Point(x, y);
+                int 
+                point = 
+
+                // – целое число, Point p (правосторонняя операция, уменьшается координата y)
+
+
+                //– Point p – вычисляется расстояние до точки p, результатом должно быть вещественное число.
+
+            }
+            catch
+            {
+                Console.WriteLine("Неверный тип данных! Должен быть double");
+            }
+        }
+    }
+}
