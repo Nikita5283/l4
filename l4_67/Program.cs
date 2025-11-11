6d6008e02f79c9343f9679e9c6ea56f4089ff6d5
Nikita <you@example.com>
2025-11-11 12:24:50 +0500

    7.9

--- l4_67/Program.cs
+++ l4_67/Program.cs
@@ -6,6 +6,7 @@
         {
             double x = 0;
             double y = 0;
+
             try
             {
                 Console.Write("Введите координату x: ");
@@ -14,40 +15,45 @@
                 y = Convert.ToDouble(Console.ReadLine());
                 Console.WriteLine();
 
+                Point point = new Point(x, y);
+
+                Console.WriteLine(point.ToString());
+                Console.WriteLine(point.Interval());
+
+                // Унарные операции
+                // Операция --
+                point = --point;
+                Console.WriteLine(point.ToString());
+
+                // Операция -
+                point = -point;
+                Console.WriteLine(point.ToString());
+
+                // Операции приведения типа
+                // Неявное приведение к Int
+                int x_int = point;
+                Console.WriteLine(x_int);
+
+                // Явное приведение к Double
+                double y_double = (double)point;
+                Console.WriteLine(y_double);
+
             }
             catch
             {
                 Console.WriteLine("Неверный тип данных! Должен быть double");
             }
 
-            Point point = new Point(x, y);
-
-            Console.WriteLine(point.ToString());
-            Console.WriteLine(point.Interval());
-
-            // Унарные операции
-            // Операция --
-            point = --point;
-            Console.WriteLine(point.ToString());
-
-            // Операция -
-            point = -point;
-            Console.WriteLine(point.ToString());
-
-            // Операции приведения типа
-            // Неявное приведение к Int
-            int x_int = point;
-            Console.WriteLine(x_int);
-
-            // Явное приведение к Double
-            double y_double = (double)point;
-            Console.WriteLine(y_double);
+            
 
             // Бинарные операции
             // – Point p, целое число (левосторонняя операция, уменьшается координата х)
             //Point point_leftx = new Point(x, y);
             try 
             {
+                Point point = new Point(x, y);
+                Console.WriteLine("Создана новая точка с координатами x, y:");
+                Console.WriteLine(point.ToString());
                 Console.Write("Введите целое число на которое нужно уменьшить координату x: ");
                 int num1 = Convert.ToInt32(Console.ReadLine());
                 point = point - num1;
@@ -69,6 +75,9 @@
             // – Point p – вычисляется расстояние до точки p, результатом должно быть вещественное число.
             try
             {
+                Point point = new Point(x, y);
+                Console.WriteLine("Создана новая точка с координатами x, y:");
+                Console.WriteLine(point.ToString());
                 Console.Write("Введите координату x2: ");
                 double x2 = Convert.ToDouble(Console.ReadLine());
                 Console.Write("Введите координату y2: ");
